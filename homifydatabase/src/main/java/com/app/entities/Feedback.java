865c1bf91f3f6c2f352c6007183b6282bd0ed0da
P. Vinay Kumar <pvinaykumar2209@gmail.com>
2025-02-11 11:39:23 +0530

    database entity design

---
+++ homifydatabase/src/main/java/com/app/entities/Feedback.java
@@ -0,0 +1,45 @@
+package com.app.entities;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="feedback")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Feedback extends BaseEntity {
+	
+	private int rating;
+	
+	@Column(length = 225)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name="customer_id")
+	private User customers;
+	
+	@ManyToOne
+	@JoinColumn(name="serviceprovider_id")
+	private ServiceProvider serviceprovider;
+
+
+	
+	
+}
